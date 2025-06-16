3ec991ede6401d9308a93ec31302b354c1c8c9cc
SelAchraf <newachraf23@gmail.com>
2025-06-16 10:44:58 +0100

    complete the expedition model

--- code/customer_odoo_apps/artec_collect/models/artec_strapping_line.py
+++ code/customer_odoo_apps/artec_collect/models/artec_strapping_line.py
@@ -1,19 +1,25 @@
-from odoo import fields, models
+from odoo import api, fields, models
+from odoo.exceptions import ValidationError
+
 
 class ArtecStrappingLine(models.Model):
     _name="artec.strapping.line"
     
     depth = fields.Float(
         string='Depth [m]',
-        required=True
     )
 
     volume = fields.Float(
         string='Volume [m3]',
-        required=True
     )
     
     strapping_id = fields.Many2one(
         comodel_name='artec.strapping',
         string='Strapping'
     )
+    
+    @api.constrains('depth', 'volume')
+    def _check_depth_volume_zero(self):
+        for record in self:
+            if (record.depth == 0.0 and record.volume != 0.0) or (record.volume == 0.0 and record.depth != 0.0):
+                raise ValidationError("If one of depth or volume is zero, the other must also be zero.")
