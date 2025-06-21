9e9ed84fe409ffd08278d846916f3296604b71ec
SelAchraf <seloughamedachraf@gmail.com>
2025-06-21 15:46:39 +0100

    make the artec_astm_line and artec_strapping_line inside each parent model

--- code/customer_odoo_apps/artec_collect/models/artec_astm.py
+++ code/customer_odoo_apps/artec_collect/models/artec_astm.py
@@ -95,3 +95,35 @@ class ArtecAstm(models.Model):
     def action_confirm(self):
         for record in self:
             record.state = 'confirmed'
+
+class ArtecAstmLine(models.Model):
+    _name="artec.astm.line"
+    
+    temperature = fields.Float(
+        string='Temperature [c°]',
+    )
+
+    density = fields.Float(
+        string='Density [sg]',
+    )
+    
+    coefficient = fields.Float(
+        string='Coefficient',
+    )
+    
+    astm_id = fields.Many2one(
+        comodel_name='artec.astm',
+        string='Astm'
+    )
+    
+    _sql_constraints = [
+        ('unique_temperature_density_astm',
+        'UNIQUE(temperature, density, astm_id)',
+        'Duplicate temperature and density combination is not allowed within the same ASTM.')
+    ]
+    
+    @api.constrains('coefficient')
+    def _check_coefficient_not_zero(self):
+        for record in self:
+            if record.coefficient == 0:
+                raise ValidationError("The coefficient must not be zero.")
